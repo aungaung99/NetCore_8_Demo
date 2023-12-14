c0c2a4afd81e18e2e5bbba94e195b53111ead153
Aung Naing Oo <aungaung20.ab@gmail.com>
2023-12-14 22:52:41 +0630

    Create StatesController.cs

---
+++ NetCore_API/Controllers/StatesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatesController : ControllerBase
+    {
+        private readonly NetCoreDemoContext _context;
+        public StatesController(NetCoreDemoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OnGetAsync()
+        {
+            return Ok(await _context.States.ToListAsync());
+        }
+    }
+}
