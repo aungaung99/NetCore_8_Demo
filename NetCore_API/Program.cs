e7404894fff9ac76258a137f8838097f42a017c3
Aung Naing Oo <aungaung20.ab@gmail.com>
2023-12-16 23:28:12 +0630

    added Serilog config

--- NetCore_API/Program.cs
+++ NetCore_API/Program.cs
@@ -1,4 +1,24 @@
+using Serilog;
+using Serilog.Events;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+var logger = Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(
+        $"C:/webLogs/NetCore_API/{DateTime.Now.Date:dd-mm-yyyy}.log",
+        restrictedToMinimumLevel: LogEventLevel.Information,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {CorrelationId} {Level:u3}] {Username} {Message:lj}{NewLine}{Exception}"
+        )
+    .CreateLogger();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
+builder.Host.UseSerilog();
+
+builder.Services.AddHealthChecks();
 
 // Add services to the container.
 
@@ -10,6 +30,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbService(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging(); // <-- Add this line
+
+app.MapHealthChecks("/healthz").RequireHost("*:7107"); ;
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
